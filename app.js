b9ca2cfabfb9eb46ebe84a0793048e3d3e5d5aa7
ryanbrown358 <ryanbrown358@gmail.com>
2018-08-23 10:45:29 -0700

    Started UI

--- app.js
+++ app.js
@@ -1,24 +1,64 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
-//passport config
+
+// Load User Model
+require('./models/User');
+
+// Passport Config
 require('./config/passport')(passport);
-// load Routes
+
+// Load Routes
+const index = require('./routes/index');
 const auth = require('./routes/auth');
+
+// Load Keys
+const keys = require('./config/keys');
+
+// Map global promises
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true
+})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 const app = express();
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout:'main'
+}));
+app.set('view engine', 'handlebars');
 
-/*Display a working send from the server*/
-app.get('/', (req, res) => {
-    res.send('this is working');
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
 });
 
-
-// use Routes
+// Use Routes
+app.use('/', index);
 app.use('/auth', auth);
 
+
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
-    console.log(`Server has started on ${port}`);
+    console.log(`Server started on port ${port}`)
 });
--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,24 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-// load the google authentication
 router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
 
-/*google call back function for logging in with google*/
-router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/'}), (req, res) => {
-    /*Successful authentication, redirect dashboard*/
-    res.redirect('/dashboard');
+router.get('/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),(req, res) => {
+        res.redirect('/dashboard');
+    });
+
+router.get('/verify', (req, res) => {
+    if(req.user){
+        console.log(req.user);
+    } else {
+        console.log('Not Auth');
+    }
 });
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
 module.exports = router;
