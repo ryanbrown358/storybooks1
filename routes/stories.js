5a19877c87ce93f5c49c7a056ce60ffe0ca3f36c
ryanbrown358 <ryanbrown358@gmail.com>
2018-08-23 22:10:32 -0700

    added more routes and UI

---
+++ routes/stories.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const { ensureAuthenticated, ensureGuest } = require('../helpers/auth');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Story = mongoose.model('stories');
+const User = mongoose.model('users');
+
+
+/*Stories index*/
+router.get('/', function(req, res) {
+   /*map the public stories to the public view*/
+    Story.find({
+        status: 'public'
+    })
+        .populate('user')
+        .then(stories => {
+            res.render('stories/index', {
+                stories: stories
+            });
+        });
+});
+
+/*show single story*/
+router.get('/show/:id', function(req, res){
+   Story.findOne({
+       _id: req.params.id
+   })
+       .populate('user')
+       .then(story => {
+           res.render('stories/show',{
+               story: story
+           });
+       });
+});
+
+/*Add story form */
+router.get('/add', ensureAuthenticated, function(req, res){
+    res.render('stories/add');
+});
+
+/*process add story*/
+router.post('/', function(req, res){
+   let allowComments;
+    if(req.body.allowComments){
+        allowComments = true;
+    } else {
+        allowComments = false;
+    }
+    const newStory = {
+        title: req.body.title,
+        body: req.body.body,
+        status: req.body.status,
+        allowComments: allowComments,
+        user: req.user.id
+    };
+
+    // Create Story
+    new Story(newStory)
+        .save()
+        .then(story => {
+            res.redirect(`/stories/show/${story.id}`);
+        });
+});
+module.exports = router;
